fdf399bcb7c896cc01cf34b92f1f2cba77205d6e
Dilip Duraiswamy <dilipduraiswamy@gmail.com>
2021-05-06 17:37:00 +0530

    my roaster design changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,6 +14,10 @@ const routes: Routes = [
     path: 'profile',
     loadChildren: () => import('./profile/profile.module').then( m => m.ProfilePageModule)
   },
+  {
+    path: 'roaster',
+    loadChildren: () => import('./roaster/roaster.module').then( m => m.RoasterPageModule)
+  },
 
 
 ];
